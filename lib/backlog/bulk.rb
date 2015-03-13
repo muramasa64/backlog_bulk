0d4e5adcd3b07e3cc1e2f0af3693cb6335b51c0c
ISOBE Kazuhiko <muramasa64@gmail.com>
2015-03-13 11:34:00 +0900

    initial commit
    bundle --bin backlog-bulk

---
+++ lib/backlog/bulk.rb
@@ -0,0 +1,7 @@
+require "backlog/bulk/version"
+
+module Backlog
+  module Bulk
+    # Your code goes here...
+  end
+end
---
+++ lib/backlog/bulk/version.rb
@@ -0,0 +1,5 @@
+module Backlog
+  module Bulk
+    VERSION = "0.1.0"
+  end
+end
